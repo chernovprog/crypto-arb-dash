e9ce3d1b84279f2244692e60a2a398741de5dc6f
Andrii <andrii.chernov@gmail.com>
2026-04-20 19:05:48 +0300

    feat(api): setup TanStack Query provider and devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 
 import { InitColorSchemeScript } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { enableMapSet } from "immer";
 import { createRoot } from 'react-dom/client'
 
@@ -12,16 +14,29 @@ import App from './App.tsx'
 
 enableMapSet();
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <InitColorSchemeScript
-      attribute="data-mui-color-scheme"
-      modeStorageKey="mui-mode"
-    />
-    <ThemeProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <InitColorSchemeScript
+        attribute="data-mui-color-scheme"
+        modeStorageKey="mui-mode"
+      />
+      <ThemeProvider>
+        <AuthProvider>
+          <App />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </AuthProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
