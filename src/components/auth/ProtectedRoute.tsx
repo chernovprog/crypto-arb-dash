f6fd9cd6a25dc6eaee40ebee553e10cb8a3ac8ff
Andrii <andrii.chernov@gmail.com>
2026-04-26 16:50:08 +0300

    feat(ui): add loader for ProtectedRoute component

--- src/components/auth/ProtectedRoute.tsx
+++ src/components/auth/ProtectedRoute.tsx
@@ -1,11 +1,12 @@
 import { Navigate, Outlet } from 'react-router-dom';
 
+import { FullPageLoader } from "@/components/common/FullPageLoader";
 import { useAuth } from "@/providers/Auth/context";
 
 const ProtectedRoute = () => {
   const { isAuthenticated, isLoading } = useAuth();
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <FullPageLoader />;
 
   return isAuthenticated ? (
     <Outlet />
